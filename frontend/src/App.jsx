add6b9f7877390d64bdf7f6ae4d6a42d1a4f1d6d
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-05-17 04:30:21 +0530

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,9 @@ import {
   Routes,
 } from "react-router-dom";
 import { Signup } from "./pages/Signup";
-// import { Signin } from "./pages/Signin";
-// import { Dashboard } from "./pages/Dashboard";
-// import { SendMoney } from "./pages/SendMoney";
+ import { Signin } from "./pages/Signin";
+ import { Dashboard } from "./pages/Dashboard";
+  import { SendMoney } from "./pages/SendMoney";
 
 function App() {
   return (
@@ -14,9 +14,9 @@ function App() {
        <BrowserRouter>
         <Routes>
           <Route path="/signup" element={<Signup />} />
-          {/* <Route path="/signin" element={<Signin />} />
+          <Route path="/signin" element={<Signin />} />
           <Route path="/dashboard" element={<Dashboard />} />
-          <Route path="/send" element={<SendMoney />} /> */}
+          <Route path="/send" element={<SendMoney />} />
         </Routes>
       </BrowserRouter>
     </>
