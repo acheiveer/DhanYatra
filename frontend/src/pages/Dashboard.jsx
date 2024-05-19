42621c186cc6ed8e1750957c9910db2bb3e92e4c
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-05-20 02:52:04 +0530

    Added balance so that logged user can see his balance on dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -2,8 +2,22 @@ import { useNavigate } from "react-router-dom"
 import { Appbar } from "../components/Appbar"
 import { Balance } from "../components/Balance"
 import { Users } from "../components/Users"
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 export const Dashboard = () => {
+    const [balance, setBalance]  = useState("")
+
+     useEffect(() => {
+      axios.get("http://localhost:3000/api/v1/account/balance", {
+        headers: {
+            Authorization: "Bearer " + localStorage.getItem("token")
+        }
+    })
+        .then(response => {
+            setBalance(parseFloat(response.data.balance).toFixed(2))
+        })
+}, [])
     const navigate = useNavigate()
     const handleLogout = () => {
         localStorage.clear();
@@ -12,7 +26,7 @@ export const Dashboard = () => {
     return <div>
         <Appbar onClick={handleLogout}/>
         <div className="m-8">
-            <Balance value={"10,000"} />
+            <Balance value={balance} />
             <Users />
         </div>
     </div>
