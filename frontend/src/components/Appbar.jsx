ca4919ca4aebb272c84ef5a0070ed0ebd4227110
prabhakarsinghssm1@gmail.com <prabhakarsinghssm1@gmail.com>
2024-05-20 01:34:49 +0530

    added Signin and Logout

--- frontend/src/components/Appbar.jsx
+++ frontend/src/components/Appbar.jsx
@@ -1,5 +1,6 @@
+import { Button } from "./Button"
 
-export const Appbar = () => {
+export const Appbar = ({onClick,name}) => {
     return <div className="shadow h-14 flex justify-between">
         <div className="flex flex-col justify-center h-full ml-4">
             PayTM App
@@ -10,9 +11,15 @@ export const Appbar = () => {
             </div>
             <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
                 <div className="flex flex-col justify-center h-full text-xl">
-                    U
+                    {}
                 </div>
             </div>
+            
+          <div>
+            <Button label={"Logout"} onClick={onClick}/>
+          </div>
+            
+          
         </div>
     </div>
 }
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,10 +1,16 @@
+import { useNavigate } from "react-router-dom"
 import { Appbar } from "../components/Appbar"
 import { Balance } from "../components/Balance"
 import { Users } from "../components/Users"
 
 export const Dashboard = () => {
+    const navigate = useNavigate()
+    const handleLogout = () => {
+        localStorage.clear();
+        navigate('/signin');
+      }
     return <div>
-        <Appbar />
+        <Appbar onClick={handleLogout}/>
         <div className="m-8">
             <Balance value={"10,000"} />
             <Users />
